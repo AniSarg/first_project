5a9f846e64eb9483b31ea89ec8ca9a06d0552c7c
Ani <ani@ani.s>
2025-02-26 17:53:34 +0400

    fix

--- techworld-js-docker-demo-app/app/server.js
+++ techworld-js-docker-demo-app/app/server.js
@@ -75,6 +75,6 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(3000,'0.0.0.0', function () {
   console.log("app listening on port 3000!");
 });
