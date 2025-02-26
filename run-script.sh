68ac11a375f5d6da84a703123a75edd2b22bf85f
Ani <ani@ani.s>
2025-02-26 12:48:16 +0400

    adding docker login

--- run-script.sh
+++ run-script.sh
@@ -1,7 +1,7 @@
-git pull https://github.com/AniSarg/first_project.git
+git clone https://github.com/AniSarg/first_project.git
 cd techworld-js-docker-demo-app
-docker login
-docker build -t anilag/web-app .
-docker tag anilag/web-app anilag/web-app:v1
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t web-app .
+docker tag web-app anilag/web-app:v1
 docker push anilag/web-app:v1
 docker-compose up  -d
